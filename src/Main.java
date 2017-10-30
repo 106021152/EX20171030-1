e4e00e00c6c875cefbf49650152b55df14bc25f3
106021152 <32092982+106021152@users.noreply.github.com>
2017-10-30 14:53:25 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int v1 = scn.nextInt();
+        int v2 = scn.nextInt();
+        System.out.println(fun1(v1)/(fun1(v2)*fun1(v1-v2)));
+    }
+
+    public static int fun1(int a) {
+        int sum = 1;
+        for (int i = a; i > 1; i--) {
+           sum =sum * i;
+            }
+
+            return sum;
+        }
+    }
